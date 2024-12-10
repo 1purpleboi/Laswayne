b608cca6cae552bb455ed8f1159c974469c239f2
1purpleboi <xlaswayne@gmail.com>
2024-12-10 21:00:56 +0700

    ID index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let userID = 'c68d755c-5220-4635-803f-52bb7c73b944';
  * Array of proxy server addresses with ports
  * Format: ['hostname:port', 'hostname:port']
  */
-const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
+const proxyIPs = ['103.133.223.51:2096'];
 
 // Randomly select a proxy server from the pool
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
